ecf8de5dc5e7a004aeaee46d4457268b2e95427a
Ghost-ai-cpu <72390639+Ghost-ai-cpu@users.noreply.github.com>
2022-12-04 20:56:13 +0530

    Fixes

--- cryptonote_config.h
+++ cryptonote_config.h
@@ -1,21 +1,21 @@
 // Copyright (c) 2014-2019, The Monero Project
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other
 //    materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //    used to endorse or promote products derived from this software without specific
 //    prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -25,7 +25,7 @@
 // INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
 #pragma once
@@ -39,6 +39,7 @@
 #define CRYPTONOTE_DNS_TIMEOUT_MS                       20000
 
 #define CRYPTONOTE_MAX_BLOCK_NUMBER                     500000000
+#define CRYPTONOTE_MAX_BLOCK_SIZE                       500000000  // block header blob limit, never used!
 #define CRYPTONOTE_MAX_TX_SIZE                          1000000
 #define CRYPTONOTE_MAX_TX_PER_BLOCK                     0x10000000
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
@@ -71,6 +72,16 @@ static_assert(STAKING_PORTIONS % 12 == 0, "Use a multiple of twelve, so that it 
 
 #define STORAGE_SERVER_PING_LIFETIME                    UPTIME_PROOF_FREQUENCY_IN_SECONDS
 #define WORKTIPSNET_PING_LIFETIME                           UPTIME_PROOF_FREQUENCY_IN_SECONDS
+
+#define  COIN                        (uint64_t)100000000 // 1 WORKTIPS = pow(10, 8)
+#define  MONEY_SUPPLY                (UINT64_C(184467440737095516)) // MONEY_SUPPLY - total number coins to be generated
+#define  EMISSION_SPEED_FACTOR_PER_MINUTE  (20)
+#define  FINAL_SUBSIDY_PER_MINUTE  ((uint64_t)300000000000) // 3 * pow(10, 11)
+#define  EMISSION_LINEAR_BASE        ((uint64_t)(1) << 58)
+#define  YEARLY_INFLATION_INVERSE    200 // 0.5% yearly inflation, inverted for integer division
+#define  EMISSION_SUPPLY_MULTIPLIER  19
+#define  EMISSION_SUPPLY_DIVISOR     10
+#define  EMISSION_DIVISOR            2000000
 
 #define CRYPTONOTE_REWARD_BLOCKS_WINDOW                 100
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2    60000 //size of block (bytes) after which reward for block calculated using block size
